e9e5b078ee42448f09f9f91d2b653c15655d872a
arjun <arjunpv0318@gmail.com>
2025-04-19 12:31:46 +0530

    window updated to standard white color

--- dig_clock.py
+++ dig_clock.py
@@ -5,6 +5,10 @@ from time import strftime
 root = tk.Tk()
 root.title("Digital Clock")
 
+# Set window size (width x height) and background color
+root.geometry("400x200")  # Width: 400px, Height: 200px
+root.configure(bg='white')  # Background color
+
 # Function to update the time on the clock
 def time():
     string = strftime('%H:%M:%S %p')  # Format for hour:minute:second AM/PM
@@ -12,10 +16,9 @@ def time():
     label.after(1000, time)  # Update the time every 1000ms (1 second)
 
 # Label to display the time
-label = tk.Label(root, font=('calibri', 50, 'bold'), background='black', foreground='yellow')
+label = tk.Label(root, font=('calibri', 50, 'bold'), background='white', foreground='yellow')
 label.pack(anchor='center')
 
-time()  # Call the time function to start the clock
 
 # Run the tkinter loop
 root.mainloop()
